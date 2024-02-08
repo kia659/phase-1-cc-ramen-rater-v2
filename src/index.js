29e86f3ade39ee349533039d18140c3996183c2b
Kia Neitzell <kia.neitzell@gmail.com>
2024-02-08 08:58:41 -0800

    compleated

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
 // index.js
+const url = `http://localhost:3000/ramens`// Add code
+const ramenName = document.querySelector('#ramen-name')
+const ramenImage = document.querySelector('#ramen-image')
+const ramenRestaurant = document.querySelector('#ramen-restaurant')
+const ramenRating = document.querySelector('#ramen-rating')
+const ramenComment = document.querySelector('#ramen-comment')
+const ramenMenu = document.querySelector('#ramen-menu')// Callbacks
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
+const handleClick = () => {
+
+}
 
 const addSubmitListener = () => {
-  // Add code
+const newRamen = document.querySelector('#new-ramen')
+newRamen.addEventListener('submit')
+
 }
 
 const displayRamens = () => {
-  // Add code
+  return fetch(url)
+  .then (resp => resp.json())
+  .then (ramenData => {
+ramenData.forEach(ramenObj => { 
+   //!creat img tag.set source to ramen img/append new img tag onto the DOM 
+  const ramenImg = document.createElement("img")
+  ramenImg.src = ramenObj.image
+  ramenImage.alt = ramenObj.name
+ramenMenu.appendChild(ramenImg)
+}); 
+handleClick()
+console.log(ramenData)
+    ramenName.innerText = ramenData[0].name
+    ramenRestaurant.innerText = ramenData[0].restaurant
+    ramenImage.src = ramenData[0].image
+    ramenImage.alt = ramenData[0].name
+    ramenComment.innerText = ramenData[0].comment
+    ramenRating.innerText = ramenData[0].rating
+    // console.log(ramenObj[0])
+  })
 };
 
 const main = () => {
   // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
+displayRamens()} // Invoke addSubmitListener here}
 
 main()
 
